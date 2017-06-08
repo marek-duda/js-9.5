2beaf26b841f6b0e15f67417f90e700063baacd2
Maro <grafika.marekduda@gmail.com>
2017-06-08 22:27:57 +0200

    add console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 var buttons = document.getElementsByClassName('button');
 
 for (var i = 0; i < buttons.length; i++) {
-  if (i == 0) {
-    alert('Przycisk 1');
-  } if (i == 1) {
-    alert('Przycisk 2');
-  } if (i == 2) {
-    alert('Przycisk 3');
-  }
-  console.log(i);
-  console.log(buttons)
+  console.log(buttons[i].innerText);
 }
